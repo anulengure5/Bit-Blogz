4d87b8d72259c423c29a23c59981d496492edadf
Anurag Lengure <anulengure5@gmail.com>
2024-03-21 00:50:52 +0530

    mongodb cloud instance connection with dotenv passHide

---
+++ API/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO) //MONGO in .env stores the url string with password and does not get shown in github repo need dotenv package 
+.then(()=>{
+    console.log('Mongodb is connected');
+}).catch(err=>
+    {
+        console.log(err);
+    })
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000!");
+}
+);
