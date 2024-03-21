bbfadf1876ba32f56941a0b06f414d5cf1ad15fc
Anurag Lengure <anulengure5@gmail.com>
2024-03-21 16:21:26 +0530

    user model / schema created

---
+++ API/models/user.model.js
@@ -0,0 +1,24 @@
+import moongose from 'moongose';
+
+const userSchema=new moongose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    }, },
+    {timestamps:true}
+    
+    );
+
+    const User=mongoose.model('User',userSchema);
+
+    export default User;
